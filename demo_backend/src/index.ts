2d4a07776cb65fc1520dc9105f714c35310e5d11
PHATHUY\troll <trollergamehuydk@gmail.com>
2026-08-13 20:00:24 +0700

    dev/update: cập nhật lệnh chạy dev và cập nhật index

--- demo_backend/src/index.ts
+++ demo_backend/src/index.ts
@@ -1,6 +1,11 @@
 import cors from 'cors'
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT ?? 3000
 app.use(cors())
+
+app.listen(port, () => {
+    console.log('Test chạy', port)
+})
